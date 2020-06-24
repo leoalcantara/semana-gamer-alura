f8913ceb32637836f652fb8a63b438583b7505e4
leoalcantara <leonidio.ti@gmail.com>
2020-06-24 12:29:25 -0300

    game sound inserted

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 let imagemCenario;
 let imagemPersonagem;
 let cenario;
+let somDojogo;
 
 function preload(){
   imagemCenario = loadImage('./imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('./imagens/personagem/correndo.png');
+  somDojogo = loadSound('./sons/trilha_jogo.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cenario = new Cenario(imagemCenario, 2.5);  
+  cenario = new Cenario(imagemCenario, 2.5); 
+  somDojogo.loop();
 }
 
 function draw() {
