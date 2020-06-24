2043b705d849cc2cdef88c968883965a0ee23cc8
leoalcantara <leonidio.ti@gmail.com>
2020-06-24 14:57:26 -0300

    refactoring the code and animating the character

--- sketch.js
+++ sketch.js
@@ -12,38 +12,15 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 2.5); 
-  somDojogo.loop();
+  personagem = new Personagem(imagemPersonagem);
+  //frameRate(40);
+  //somDojogo.loop();
 }
 
 function draw() {
   cenario.exibe();
   cenario.move();
-  image(imagemPersonagem, 0, height -135, 110, 135, 0, 0, 220, 270);
+  personagem.exibe();
+  
 }
 
-class Cenario {
-  constructor(imagem, velocidade){
-    this.imagem = imagem;
-    this.velocidade = velocidade;
-    this.x1 = 0;
-    this.x2 =  width;
-}
-
-exibe(){
-  image(this.imagem, this.x1, 0, width, height);
-  image(this.imagem, this.x2, 0, width, height);
-}
-
-move(){
-  this.x1 = this.x1 - this.velocidade;
-  this.x2 = this.x2 - this.velocidade;
-
-  if(this.x1 < -width){
-    this.x1 = width;
-  }
-  if(this.x2 < -width){
-    this.x2 = width;
-  }
-}
-
-}
